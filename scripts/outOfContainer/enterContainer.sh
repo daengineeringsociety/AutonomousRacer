5e76c7e936c82b06ae8085de04617be6b33a449b
ion206 <ayansyedca@gmail.com>
2026-02-08 05:44:25 +0000

    switch to rosFoxy

--- scripts/outOfContainer/enterContainer.sh
+++ scripts/outOfContainer/enterContainer.sh
@@ -6,7 +6,7 @@ CONTAINER_ID="autonomousracer_devcontainer-ros_dev-1"
 if [ "$(docker inspect -f '{{.State.Running}}' "$CONTAINER_ID" 2>/dev/null)" = "true" ]; then
     echo "Container $CONTAINER_ID is running1"
     echo "Entering Container!"
-    docker exec -it -u ros -w /workspaces/deepracer_project $CONTAINER_ID "bash"
+    docker exec -it -u daes -w /workspaces/deepracer_project $CONTAINER_ID "bash"
 else
     echo "Container $CONTAINER_ID is not running."
 fi
