9d1bab01dc6253ff1a46457e45d8f89977fbeaf6
ion206 <ayansyedca@gmail.com>
2026-02-26 22:32:58 +0000

    clone in deepracer code

--- scripts/imageControl/runContainer.sh
+++ scripts/imageControl/runContainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/deepracer/deepracer/AutonomousRacer
 echo "Running: autonomousracer_devcontainer_ros_dev-1"
-docker run -it --rm   --name autonomousracer_devcontainer_ros_dev-1   --privileged   --network host   --cap-add NET_ADMIN   --device /dev/net/tun:/dev/net/tun   -v "$(pwd):/workspaces/deepracer_project"   -u daes   autonomousracer_devcontainer_ros_dev bash
+docker run -it --rm   --name autonomousracer_devcontainer_ros_dev-1   --privileged   --network host   --cap-add NET_ADMIN   --device /dev/net/tun:/dev/net/tun   -v "$(pwd):/workspaces/deepracer_project"  -v /sys/class/pwm:/sys/class/pwm -u daes   autonomousracer_devcontainer_ros_dev bash
